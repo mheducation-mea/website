2d1ce963fc67ca1f89e40efa28f0b818b116bd11
mheducation-mea <126054969+mheducation-mea@users.noreply.github.com>
2023-04-11 17:01:10 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'arrow3.json'
+  })
+
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm-mobi'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'arrow-mobile2.json'
+  })
