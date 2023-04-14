82cdc51468b0f28f2ff5699eaf59ea1e90544971
mheducation-mea <126054969+mheducation-mea@users.noreply.github.com>
2023-04-14 15:18:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: 'https://mheducation-mea.github.io/website/arrow3.json'
-  })
-
-
-  var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm-mobi'),
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: 'https://mheducation-mea.github.io/website/arrow-mobile2.json'
-  })
+  define(['lottie'],function($){
+    var animation = bodymovin.loadAnimation({
+      container: document.getElementById('bm'),
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: 'arrow3.json'
+    })
+  
+  
+    var animation = bodymovin.loadAnimation({
+      container: document.getElementById('bm-mobi'),
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: 'arrow-mobile2.json'
+    })
+  });
