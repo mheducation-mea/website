d4d8f4d4dc564e81b43ea838e12a3feb6a52fe6c
mheducation-mea <126054969+mheducation-mea@users.noreply.github.com>
2023-04-14 15:35:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 requirejs.config({
     paths:{
-        lottie:['https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.10.2/lottieee.min']
+        lottie:['https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.10.2/lottie.min']
     }
 });
